58c1f58e928822a6963b499f17eab26dcac95ffc
Arthur Moreira <tutu.moreira.correa@gmail.com>
2024-06-07 01:49:34 -0300

    Refactor Manga class and related classes for better code organization and readability

--- mangahandler/src/main/java/manga/Main.java
+++ mangahandler/src/main/java/manga/Main.java
@@ -2,7 +2,15 @@ package manga;
 
 import javax.swing.SwingUtilities;
 
+/**
+ * Main class
+ * It is the entry point of the application
+ */
 public class Main {
+    /**
+     * Main method to start the application
+     * @param args Arguments
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
--- mangahandler/src/main/java/manga/TitleIndexEntry.java
+++ mangahandler/src/main/java/manga/TitleIndexEntry.java
@@ -1,18 +1,37 @@
 package manga;
 
+/**
+ * TitleIndexEntry class
+ * It represents a title index entry object with its attributes
+ * - title: Title
+ * - isbn: International Standard Book Number
+ */
 public class TitleIndexEntry {
     private String title;
     private String isbn;
 
+    /**
+     * Constructor
+     * @param title Title
+     * @param isbn ISBN
+     */
     public TitleIndexEntry(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
     }
 
+    /**
+     * Get the title
+     * @return Title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Get the ISBN
+     * @return ISBN
+     */
     public String getIsbn() {
         return isbn;
     }
